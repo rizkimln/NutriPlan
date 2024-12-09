03a690b0f2f5ffba3e771957bc3b7411d5ce8b27
rizkimln <rizkimln30@gmail.com>
2024-12-09 08:10:34 +0700

    perubahan dan nambahin file halo.txt

--- app/src/main/java/com/example/tugashalamanawal/Login.kt
+++ app/src/main/java/com/example/tugashalamanawal/Login.kt
@@ -1,20 +1,59 @@
 package com.example.tugashalamanawal
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.firebase.auth.FirebaseAuth
 
 class Login : AppCompatActivity() {
+
+    private lateinit var firebaseAuth: FirebaseAuth
+    private lateinit var usernameuser : EditText
+    private lateinit var emailuser : EditText
+    private lateinit var passworduser : EditText
+    private lateinit var battonmasuk : Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_login)
+
+        emailuser = findViewById(R.id.editEmail)
+        passworduser = findViewById(R.id.editPassword)
+        battonmasuk = findViewById(R.id.btnMasuk)
+        firebaseAuth = FirebaseAuth.getInstance()
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        battonmasuk.setOnClickListener(View.OnClickListener {
+            var email=emailuser.text.toString().trim()
+            var password=passworduser.text.toString().trim()
+            if (email.isEmpty()||password.isEmpty()){
+                Toast.makeText(applicationContext, "ga boleh kosong", Toast.LENGTH_SHORT).show()
+            }
+            else {
+                firebaseAuth.signInWithEmailAndPassword(email,password).addOnCompleteListener(this){task->
+                    if(task.isComplete){
+                        Toast.makeText(applicationContext, "berhasil", Toast.LENGTH_SHORT).show()
+                        startActivity(Intent(this,MainActivity::class.java))
+
+                    } else{
+                        Toast.makeText(applicationContext, "gagal", Toast.LENGTH_SHORT).show()
+                    }
+
+                }
+            }
+        })
     }
 }
--- app/src/main/java/com/example/tugashalamanawal/Register.kt
+++ app/src/main/java/com/example/tugashalamanawal/Register.kt
@@ -1,5 +1,6 @@
 package com.example.tugashalamanawal
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
@@ -47,6 +48,7 @@ class Register : AppCompatActivity() {
                 firebaseAuth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(this){task->
                     if(task.isComplete){
                         Toast.makeText(applicationContext, "berhasil", Toast.LENGTH_SHORT).show()
+                        startActivity(Intent(this,Login::class.java))
 
                     } else{
                         Toast.makeText(applicationContext, "gagal", Toast.LENGTH_SHORT).show()
