401bc4a38f059c04bb6d057bc77af3f77da30cee
rizkimln <rizkimln30@gmail.com>
2024-12-16 22:55:44 +0700

    nambah sdk firestore

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,6 +45,7 @@ dependencies {
     implementation("com.google.firebase:firebase-auth:22.0.0")
     implementation(libs.androidx.constraintlayout)
     implementation(libs.firebase.auth)
+    implementation(libs.firebase.firestore)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
