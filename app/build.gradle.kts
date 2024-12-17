fc300a6963c6ddfd73ef0b67cbd46da93288860b
rizkimln <rizkimln30@gmail.com>
2024-12-17 09:27:27 +0700

    nambah databse firestore sudah berhasil

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -47,6 +47,8 @@ dependencies {
     implementation(libs.androidx.constraintlayout)
     implementation(libs.firebase.auth)
     implementation(libs.firebase.database.ktx)
+    implementation(libs.firebase.firestore)
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
