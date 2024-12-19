0bd0d566339812bd747be0a4bfd6f9816d14cdd3
Mozanda00 <mozandatiarad@gmail.com>
2024-12-20 00:53:40 +0700

    xml isi resep

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,6 +40,7 @@ dependencies {
     implementation ("com.google.android.gms:play-services-maps:18.1.0")
     implementation ("com.google.android.gms:play-services-location:21.0.1")
     implementation("com.google.android.gms:play-services-basement:18.2.0")
+    implementation ("androidx.cardview:cardview:1.0.0")
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
