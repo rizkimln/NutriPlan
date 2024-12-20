280deaba722e5df1d231a3dd95dd3661b2b9d691
rizkimln <rizkimln30@gmail.com>
2024-12-21 02:05:06 +0700

    halaman kalkulator

--- app/src/main/java/com/example/tugashalamanawal/KalkulatorActivity.kt
+++ app/src/main/java/com/example/tugashalamanawal/KalkulatorActivity.kt
@@ -12,12 +12,12 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class KalkulatorActivity : AppCompatActivity() {
-    @SuppressLint("MissingInflatedId")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_kalkulator)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.KalkulatorBMI)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
