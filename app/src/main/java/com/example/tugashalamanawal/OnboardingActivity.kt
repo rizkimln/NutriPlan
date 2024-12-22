c3b50c126dbd0b1761fbd02cbb73cc0351a78418
rizkimln <rizkimln30@gmail.com>
2024-12-22 21:28:16 +0700

    OnBoarding sudah jadi

--- app/src/main/java/com/example/tugashalamanawal/OnboardingActivity.kt
+++ app/src/main/java/com/example/tugashalamanawal/OnboardingActivity.kt
@@ -1,20 +1,47 @@
 package com.example.tugashalamanawal
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.button.MaterialButton
 
 class OnboardingActivity : AppCompatActivity() {
+
+    private lateinit var Masuk : MaterialButton
+    private lateinit var nextRegister : MaterialButton
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_onboarding)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+
+        Masuk = findViewById(R.id.btnMasuk)
+        nextRegister = findViewById(R.id.btnDaftar)
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.OnBoarding)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        Masuk.setOnClickListener {
+            val intent = Intent(this, Login::class.java)
+            startActivity(intent)
+            finish()
+        }
+
+        nextRegister.setOnClickListener {
+            val intent = Intent(this, Register::class.java)
+            startActivity(intent)
+            finish()
+        }
+
     }
+
+
+
+
 }
